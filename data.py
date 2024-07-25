405233b3addeb2918e1c5c0067f8ff28f3a4a911
lennart <lennart.koehler@uni-jena.de>
2024-07-25 14:06:46 +0200

    data_change

--- data.py
+++ data.py
@@ -1,10 +1,12 @@
 import torch
 from torch.utils.data import Dataset
+import numpy as np
+
 
 import h5py
 import os
 
-class ReadsDataset(Dataset):
+class SquiggleDataset(Dataset):
 
     def __init__(self, root_dir, transform=None):
         self.root_dir = root_dir
@@ -15,11 +17,24 @@ class ReadsDataset(Dataset):
     def __len__(self):
         return len(self.files_list)
 
-
     def __getitem__(self, idx):
-        return h5py.File(self.root_dir + self.files_list[idx], 'r')["Raw"]["Signal"][:]
+        f5file = h5py.File(self.root_dir + self.files_list[idx], 'r')
+        read_number = list(f5file["Raw"]["Reads"].keys())[0]
+        return f5file["Raw"]["Reads"][read_number]["Signal"][:]
 
-dataset = ReadsDataset(f"/home/lennart/Projektmodul/ecoli/tombo/fast5_files_gzip")
+
+def save_squiggle_segments(read: h5py.File, read_name, segment_length, segment_overlap, output_dir):
+    read_number = list(read["Raw"]["Reads"].keys())[0]
+    signal = read["Raw"]["Reads"][read_number]["Signal"][:]
+    signal_length = len(signal)
+    for i in range(0, signal_length, segment_length - segment_overlap):
+        if i + segment_length > signal_length:
+            break
+        segment = signal[i:i + segment_length]
+        segment_name = f"{output_dir}/{read_name}_{i}_{i + segment_length}.npy"
+        np.save(segment_name, segment)
+
+#dataset = ReadsDataset(f"/home/lennart/Projektmodul/ecoli/tombo/fast5_files_gzip/")
 
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=10, shuffle=True)
 
