fd979cd6239c7b8f29964426e069efd798a829d4
lennart <lennart.koehler@uni-jena.de>
2024-11-05 12:03:34 +0100

    added training with cuda graphs

--- dataloader_speed_test.py
+++ dataloader_speed_test.py
@@ -4,7 +4,7 @@ import time
 import torch
 
 start_time = time.time()
-path = "data_segments_reduced.h5"
+path = "data_segments.h5"
 dataset = data.H5Dataset_old(path)
 
 batch_size = 20
@@ -12,31 +12,10 @@ num_workers = 5
 
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
 
+for x in dataloader:
+    print(x)
 
 
 print(time.time()-start_time)
 
-def h5_tree(val, pre=''):
-    items = len(val)
-    for key, val in val.items():
-        items -= 1
-        if items == 0:
-            # the last item
-            if type(val) == h5py._hl.group.Group:
-                print(pre + '└── ' + key)
-                h5_tree(val, pre+'    ')
-            else:
-                try:
-                    print(pre + '└── ' + key + ' (%d)' % len(val))
-                except TypeError:
-                    print(pre + '└── ' + key + ' (scalar)')
-        else:
-            if type(val) == h5py._hl.group.Group:
-                print(pre + '├── ' + key)
-                h5_tree(val, pre+'│   ')
-            else:
-                try:
-                    print(pre + '├── ' + key + ' (%d)' % len(val))
-                except TypeError:
-                    print(pre + '├── ' + key + ' (scalar)')
 
